23a80eb255583000a291ce41a1b88f4059479c3f
Ubuntu <ubuntu@ip-172-31-71-25.ec2.internal>
2020-11-17 16:27:15 +0000

    added test case for confirming successful conversion

--- test.sh
+++ test.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+echo " "
+
 echo "Beginning tests..."
 
 echo " "
 
 echo "No arguments given - expecting no output"
 
-javac Dec2Hex.java
+java Dec2Hex.java
+
+echo " "
+
+echo "Argument 15 given - expecting F"
+
+java Dec2Hex.java 15
+
+echo " "
