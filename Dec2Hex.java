a3e4f24e7afe902d4ff965bfdcbb795d5b3646d9
Ubuntu <ubuntu@ip-172-31-71-25.ec2.internal>
2020-11-20 13:41:47 +0000

    added if statement to catch and exit if no arguments given

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,6 +7,12 @@ class Dec2Hex
 
     public static void main(String args[])
     {
+
+	if(args.length <= 0){
+	  System.out.println("No decimal value given - exiting program...");
+	  System.exit(0);
+	}
+
         if (args.length > 0) 
         {
             try 
