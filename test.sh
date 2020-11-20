6d3acf57edd206b742baf7b291246cfe4ab7e1cc
Ubuntu <ubuntu@ip-172-31-71-25.ec2.internal>
2020-11-20 14:21:49 +0000

    remove java extension to fix class error

--- test.sh
+++ test.sh
@@ -10,43 +10,43 @@ echo " "
 
 echo "No arguments given - expecting no output"
 
-java Dec2Hex.java
+java Dec2Hex
 
 echo " "
 
 echo "Argument 15 given - expecting F"
 
-java Dec2Hex.java 15
+java Dec2Hex 15
 
 echo " "
 
 echo "Argument 456 given - expecting 1C8"
 
-java Dec2Hex.java 456
+java Dec2Hex 456
 
 echo " "
 
 echo "Argument 0 given - expecting 0"
 
-java Dec2Hex.java 0
+java Dec2Hex 0
 
 echo " "
 
 echo "String 'Hello!' given - error should be returned"
 
-java Dec2Hex.java 'Hello'
+java Dec2Hex 'Hello'
 
 echo " "
 
 echo "Double 2.5 given - error should be returned"
 
-java Dec2Hex.java 2.5
+java Dec2Hex 2.5
 
 echo " "
 
 echo "Negative value -15 given - F should be returned"
 
-java Dec2Hex.java -15
+java Dec2Hex -15
 
 echo " "
 
